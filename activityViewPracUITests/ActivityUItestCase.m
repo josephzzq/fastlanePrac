7955a12c5f770cd4a61956c5e8e9b81d585c5055
josephzzq <victoryandjoseph@gmail.com>
2016-08-05 10:16:22 +0800

    DESC: add snapshot test result and snapshot file

--- activityViewPracUITests/ActivityUItestCase.m
+++ activityViewPracUITests/ActivityUItestCase.m
@@ -1,18 +1,20 @@
 //
-//  activityViewPracUITests.m
-//  activityViewPracUITests
+//  ActivityUItestCase.m
+//  activityViewPrac
 //
-//  Created by Joseph.Tsai on 2016/3/15.
+//  Created by Joseph.Tsai on 2016/7/25.
 //  Copyright © 2016年 Joseph.Tsai. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
+#import "activityViewPrac-swift.h"
 
-@interface activityViewPracUITests : XCTestCase
+
+@interface ActivityUItestCase : XCTestCase
 
 @end
 
-@implementation activityViewPracUITests
+@implementation ActivityUItestCase
 
 - (void)setUp {
     [super setUp];
@@ -23,7 +25,7 @@
     self.continueAfterFailure = NO;
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
     [[[XCUIApplication alloc] init] launch];
-    
+
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
 
@@ -34,6 +36,22 @@
 
 - (void)testExample {
     // Use recording to get started writing UI tests.
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"Share"] tap];
+    
+    XCUIElementQuery *sheetsQuery = app.sheets;
+        [Snapshot snapshot:@"fu1" waitForLoadingIndicator:YES];
+    
+    [[sheetsQuery.collectionViews.cells.collectionViews containingType:XCUIElementTypeButton identifier:@"Mail"].buttons[@"More"] tap];
+        [Snapshot snapshot:@"fu2" waitForLoadingIndicator:YES];
+    
+    [app.navigationBars[@"Activities"].buttons[@"Done"] tap];
+        [Snapshot snapshot:@"fu3" waitForLoadingIndicator:YES];
+    
+    [sheetsQuery.buttons[@"Cancel"] tap];
+    
+
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
 
